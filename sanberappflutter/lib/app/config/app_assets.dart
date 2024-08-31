b25f956c35dc22b2e77fa9e7cb37ee42c458a34b
balyun taja <balyuntaja28@gmail.com>
2024-08-31 15:42:06 +0700

    commit tugas

---
+++ sanberappflutter/lib/app/config/app_assets.dart
@@ -0,0 +1,16 @@
+class AppAssets {
+  static const image = 'assets/image';
+  static const icon = 'assets/icon';
+
+  //TODO:Image
+  static const imageNews = '$image/image_news.png';
+  static const image1 = '$image/image_1.png';
+  static const image2 = '$image/image_2.png';
+  static const image3 = '$image/image_3.png';
+  static const image4 = '$image/image_4.png';
+  static const image5 = '$image/image_5.png';
+
+  //TODO:ICON
+  static const iconMenu = '$icon/icon_menu.png';
+  static const iconArrow = '$icon/icon_arrow.png';
+}
---
+++ sanberappflutter/lib/app/config/app_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color blackColor = Color(0xFF000000);
+const Color blueColor = Color(0xFF3498DB);
+const Color whiteColor = Color(0xFFFFFFFF);
